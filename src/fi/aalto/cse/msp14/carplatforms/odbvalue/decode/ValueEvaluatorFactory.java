dddd114ae9a31f0f4bdf3c6b1da7610caa8c6651
Jarkko Pöyry <jarkko.poyry@gmail.com>
2014-04-26 16:09:53 +0300

    Don't use reflection in element parse.

--- src/fi/aalto/cse/msp14/carplatforms/odbvalue/decode/ValueEvaluatorFactory.java
+++ src/fi/aalto/cse/msp14/carplatforms/odbvalue/decode/ValueEvaluatorFactory.java
@@ -1,64 +1,50 @@
 package fi.aalto.cse.msp14.carplatforms.odbvalue.decode;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
-
 import org.w3c.dom.Element;
 
+import fi.aalto.cse.msp14.carplatforms.odbvalue.decode.evaluators.*;
+
 public class ValueEvaluatorFactory {
-	static private HashMap<String, Constructor<?>> m_evaluatorConstructionRegistry = createConstructionRegistry();
-	
 	static public IValueEvaluator parse (final Element e, final OBDSourceDefinition srcDef) throws EvaluatorParseError {
-		// check the registry
-		final Constructor<?> evaluatorConstructor = m_evaluatorConstructionRegistry.get(e.getTagName()); 
-		if (evaluatorConstructor == null)
-			new EvaluatorParseError("unknown element " + e.getTagName());
-		
-		// construct
-		try {
-			return (IValueEvaluator) evaluatorConstructor.newInstance(e, srcDef);
-		} catch (InvocationTargetException ex) {
-			// constructor failed, detect parse errors and extract them
-			if (ex.getCause() != null && ex.getCause() instanceof EvaluatorParseError)
-				throw (EvaluatorParseError)ex.getCause();
-			
-			// should never happen, just forward as RuntimeException
-			throw new RuntimeException("failed to constuct evaluation tree", ex);
-		} catch (Exception ex) {
-			// should never happen, just forward as RuntimeException
-			throw new RuntimeException("failed call constructor", ex);
-		}
-	}
-	
-	static private synchronized HashMap<String, Constructor<?>> createConstructionRegistry() {
-		final HashMap<String, Constructor<?>> registry = new HashMap<String, Constructor<?>>();
-		
-		try {
-			registry.put("mul", 			Class.forName("fi.aalto.cse.msp14.carplatforms.odbvalue.decode.evaluators.MulValueEvaluator").getConstructor(Element.class, OBDSourceDefinition.class));
-			registry.put("div", 			Class.forName("fi.aalto.cse.msp14.carplatforms.odbvalue.decode.evaluators.DivValueEvaluator").getConstructor(Element.class, OBDSourceDefinition.class));
-			registry.put("add", 			Class.forName("fi.aalto.cse.msp14.carplatforms.odbvalue.decode.evaluators.AddValueEvaluator").getConstructor(Element.class, OBDSourceDefinition.class));
-			registry.put("sub", 			Class.forName("fi.aalto.cse.msp14.carplatforms.odbvalue.decode.evaluators.SubValueEvaluator").getConstructor(Element.class, OBDSourceDefinition.class));
-			registry.put("or",  			Class.forName("fi.aalto.cse.msp14.carplatforms.odbvalue.decode.evaluators.OrValueEvaluator").getConstructor(Element.class, OBDSourceDefinition.class));
-			registry.put("xor", 			Class.forName("fi.aalto.cse.msp14.carplatforms.odbvalue.decode.evaluators.XorValueEvaluator").getConstructor(Element.class, OBDSourceDefinition.class));
-			registry.put("and", 			Class.forName("fi.aalto.cse.msp14.carplatforms.odbvalue.decode.evaluators.AndValueEvaluator").getConstructor(Element.class, OBDSourceDefinition.class));
-			registry.put("equal", 			Class.forName("fi.aalto.cse.msp14.carplatforms.odbvalue.decode.evaluators.EqualValueEvaluator").getConstructor(Element.class, OBDSourceDefinition.class));
-			registry.put("notEqual", 		Class.forName("fi.aalto.cse.msp14.carplatforms.odbvalue.decode.evaluators.NotEqualValueEvaluator").getConstructor(Element.class, OBDSourceDefinition.class));
-			registry.put("greater", 		Class.forName("fi.aalto.cse.msp14.carplatforms.odbvalue.decode.evaluators.GreaterValueEvaluator").getConstructor(Element.class, OBDSourceDefinition.class));
-			registry.put("greaterOrEqual", 	Class.forName("fi.aalto.cse.msp14.carplatforms.odbvalue.decode.evaluators.GreaterOrEqualValueEvaluator").getConstructor(Element.class, OBDSourceDefinition.class));
-			registry.put("less", 			Class.forName("fi.aalto.cse.msp14.carplatforms.odbvalue.decode.evaluators.LessValueEvaluator").getConstructor(Element.class, OBDSourceDefinition.class));
-			registry.put("lessOrEqual", 	Class.forName("fi.aalto.cse.msp14.carplatforms.odbvalue.decode.evaluators.LessOrEqualValueEvaluator").getConstructor(Element.class, OBDSourceDefinition.class));
-			registry.put("float", 			Class.forName("fi.aalto.cse.msp14.carplatforms.odbvalue.decode.evaluators.ConstantFloatValueEvaluator").getConstructor(Element.class, OBDSourceDefinition.class));
-			registry.put("integer", 		Class.forName("fi.aalto.cse.msp14.carplatforms.odbvalue.decode.evaluators.ConstantIntValueEvaluator").getConstructor(Element.class, OBDSourceDefinition.class));
-			registry.put("toFloat", 		Class.forName("fi.aalto.cse.msp14.carplatforms.odbvalue.decode.evaluators.ToFloatValueEvaluator").getConstructor(Element.class, OBDSourceDefinition.class));
-			registry.put("decodeFailure", 	Class.forName("fi.aalto.cse.msp14.carplatforms.odbvalue.decode.evaluators.DecodeFailureValueEvaluator").getConstructor(Element.class, OBDSourceDefinition.class));
-			registry.put("inputByte", 		Class.forName("fi.aalto.cse.msp14.carplatforms.odbvalue.decode.evaluators.InputByteValueEvaluator").getConstructor(Element.class, OBDSourceDefinition.class));
-			registry.put("cond", 			Class.forName("fi.aalto.cse.msp14.carplatforms.odbvalue.decode.evaluators.ConditionalValueEvaluator").getConstructor(Element.class, OBDSourceDefinition.class));
-		} catch (Exception ex) {
-			// should never happen, just forward as RuntimeException
-			throw new RuntimeException("failed to constuct registry", ex);
-		}
-		
-		return registry;
+		if ("mul".equals(e.getTagName()))
+			return new MulValueEvaluator(e, srcDef);
+		else if ("div".equals(e.getTagName()))
+			return new DivValueEvaluator(e, srcDef);
+		else if ("add".equals(e.getTagName()))
+			return new AddValueEvaluator(e, srcDef);
+		else if ("sub".equals(e.getTagName()))
+			return new SubValueEvaluator(e, srcDef);
+		else if ("or".equals(e.getTagName()))
+			return new OrValueEvaluator(e, srcDef);
+		else if ("xor".equals(e.getTagName()))
+			return new XorValueEvaluator(e, srcDef);
+		else if ("and".equals(e.getTagName()))
+			return new AndValueEvaluator(e, srcDef);
+		else if ("equal".equals(e.getTagName()))
+			return new EqualValueEvaluator(e, srcDef);
+		else if ("notEqual".equals(e.getTagName()))
+			return new NotEqualValueEvaluator(e, srcDef);
+		else if ("greater".equals(e.getTagName()))
+			return new GreaterValueEvaluator(e, srcDef);
+		else if ("greaterOrEqual".equals(e.getTagName()))
+			return new GreaterOrEqualValueEvaluator(e, srcDef);
+		else if ("less".equals(e.getTagName()))
+			return new LessValueEvaluator(e, srcDef);
+		else if ("lessOrEqual".equals(e.getTagName()))
+			return new LessOrEqualValueEvaluator(e, srcDef);
+		else if ("float".equals(e.getTagName()))
+			return new ConstantFloatValueEvaluator(e, srcDef);
+		else if ("integer".equals(e.getTagName()))
+			return new ConstantIntValueEvaluator(e, srcDef);
+		else if ("toFloat".equals(e.getTagName()))
+			return new ToFloatValueEvaluator(e, srcDef);
+		else if ("decodeFailure".equals(e.getTagName()))
+			return new DecodeFailureValueEvaluator(e, srcDef);
+		else if ("inputByte".equals(e.getTagName()))
+			return new InputByteValueEvaluator(e, srcDef);
+		else if ("cond".equals(e.getTagName()))
+			return new ConditionalValueEvaluator(e, srcDef);
+
+		throw new EvaluatorParseError("unknown element " + e.getTagName());
 	}
 }
